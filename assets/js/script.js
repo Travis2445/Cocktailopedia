d84320c0906c7b037ee072cfc81c043746b6ceee
Optical23 <40125489+Optical23@users.noreply.github.com>
2021-11-29 14:33:45 -0600

    Added the basics for landing page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,10 @@
-//
+///8c63c426femshe9811d1e49b1267p1f0839jsn4563f7d2b10f
 var currentCocktail = "";
-function buildCockTailPage(){
-    
-randomCocktail();
+var currentCocktailIMG = "";
 
-
-//Building landing page
 var mainContainer = document.querySelector("main");
-mainContainer.setAttribute("class", "row");
-mainContainer.setAttribute("style", "height:600px");
 
-var cocktailImg = document.createElement("img");
-cocktailImg.setAttribute("class", "col s4");
-var cocktailStepsContainer = document.createElement("div");
-cocktailStepsContainer.setAttribute("class", "col s4");
-console.log(currentCocktail);
-
-var cocktailStepsList = document.createElement("ul");
-
-//8c63c426femshe9811d1e49b1267p1f0839jsn4563f7d2b10f
-
-}
+randomCocktail();
 
 function randomCocktail(){
 fetch("https://the-cocktail-db.p.rapidapi.com/random.php", {
@@ -31,8 +15,13 @@ fetch("https://the-cocktail-db.p.rapidapi.com/random.php", {
 	}
 })
 .then(response => {
+	//After converting it to json set global scope functions to values to use when building the page
 	response.json().then(function(data) {
 		currentCocktail = data.drinks[0].strDrink;
+		currentCocktailIMG = data.drinks[0].strDrinkThumb;
+		//after getting the data we call the build function
+		//if we call the fetch function in the buildpage function it doesn't catch the data before running
+		buildCockTailPage();
 		console.log(data);
 	})
 })
@@ -41,10 +30,29 @@ fetch("https://the-cocktail-db.p.rapidapi.com/random.php", {
 });
 }
 
-buildCockTailPage();
-//this is how the data turns managable 
-// .then(response => {
-// 	response.json().then(function(data) {
-// 		console.log(data);
-// 	})
-// })
+function buildCockTailPage(){
+
+//Building landing page
+//mainContainer is already grabbed styled after the functions called
+mainContainer.setAttribute("class", "row");
+mainContainer.setAttribute("style", "height:600px");
+
+//Img creating and appending
+var cocktailImg = document.createElement("img");
+cocktailImg.setAttribute("class", "col s4");
+cocktailImg.setAttribute("src", currentCocktailIMG);
+mainContainer.appendChild(cocktailImg);
+
+//work in progress
+//But same idea as the image
+var cocktailStepsContainer = document.createElement("div");
+cocktailStepsContainer.setAttribute("class", "col s4");
+console.log(currentCocktail);
+var cocktailStepsHeader = document.createElement("h1");
+cocktailStepsHeader.textContent = currentCocktail;
+cocktailStepsContainer.appendChild(cocktailStepsHeader);
+mainContainer.appendChild(cocktailStepsContainer);
+
+
+}
+
